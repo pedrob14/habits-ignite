e253b3c257130875f49b3d865050a86e36cb0a47
pedrob14 <pedrob14farias@gmail.com>
2023-02-01 09:46:57 -0300

    Importando fastify e fazendo o 1º projeto com este framework

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log("Oi")
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+/**
+ * Método HTTP: Get, Post, Put, Patch, Delete
+ */
+app.get('/', () => {
+    return 'Hello World'
+})
+
+app.listen({
+    port: 3333
+})
