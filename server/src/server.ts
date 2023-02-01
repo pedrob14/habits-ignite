4cda060db5322beb520d4f036fac8b4c6968ebd5
pedrob14 <pedrob14farias@gmail.com>
2023-01-31 21:04:17 -0300

    Executando o código Typescript

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,1 @@
-type Usuario = {
-    idade: number
-}
-
-function mostraIdadeDoUsuario(usuario: Usuario) {
-    return usuario.idade
-}
-
-const usuario = {
-    name: "Diego",
-}
-
-mostraIdadeDoUsuario(usuario)
+console.log("Oi")
