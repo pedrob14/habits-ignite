d8e8261d8b3f7142de8a66264cb02c381c1f6bec
pedrob14 <pedrob14farias@gmail.com>
2023-02-02 07:32:02 -0300

    Criando a primeira rota da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
+// Importação do fastify
 import Fastify from 'fastify'
-
+// Criando a aplicação com fastify
 const app = Fastify()
 
 /**
  * Método HTTP: Get, Post, Put, Patch, Delete
  */
-app.get('/', () => {
+// Criando a rota desta aplicação
+app.get('/hello', () => {
     return 'Hello World'
 })
-
+// fazendo com que a aplicação ouça a porta 3333
 app.listen({
     port: 3333
 })
