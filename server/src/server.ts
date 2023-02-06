0f0a735535a247da84eb4f9460ec0917d7e0e792
pedrob14 <pedrob14farias@gmail.com>
2023-02-06 07:57:01 -0300

    Conexão com o banco de dados
    Fazendo a conexão com o banco de dados através do Prisma, acessando a tabela criada

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,27 @@
 // Importação do fastify
 import Fastify from 'fastify'
+// Importação do PrismaClient
+import { PrismaClient } from '@prisma/client'
 // Criando a aplicação com fastify
 const app = Fastify()
-
+// Ter acesso a todas as tabelas do banco de dados
+const prisma = new PrismaClient()
 /**
  * Método HTTP: Get, Post, Put, Patch, Delete
  */
 // Criando a rota desta aplicação
-app.get('/hello', () => {
-    return 'Hello World'
+app.get('/hello', async () => {
+    // prisma.habit -> Acessando a tabela habits através do Prisma
+    // findMany() -> porque quero retornar vários hábitos de dentro do meu banco de dados
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
 })
 // fazendo com que a aplicação ouça a porta 3333
 app.listen({
