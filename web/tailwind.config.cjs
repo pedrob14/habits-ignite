759893e8ded21e5ab5a95cad83a73aebb317c99c
pedrob14 <pedrob14farias@gmail.com>
2023-02-14 08:47:58 -0300

    Propriedades do Tailwind CSS

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // ** -> Tudo/Qualquer pasta que tiver
+    // *.tsx -> Arquivos que terminarem com .tsx
+    './src/**/*.tsx',
+    './index.html'
+  ],
   theme: {
     extend: {},
   },
