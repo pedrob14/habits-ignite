8eaa54642228573e97e28036c1d36088d78503b4
pedrob14 <pedrob14farias@gmail.com>
2023-02-14 09:24:35 -0300

    Configurando o Tailwind CSS
    Entendendo que com o Tailwind CSS podemos escrever o css da aplicação tudo através de classes.

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -12,6 +12,11 @@ export function Habit(props: HabitProps) {
     return (
         // Quando eu quero mostrar uma variável JavaScript
         //dentro do HTML e não só texto, é preciso está por volta de chaves
-        <div className='bg-zinc-900'>{props.completed}</div>
+        // w -> width (Largura)
+        // h -> height (Altura)
+        // m -> margin
+        <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center'>
+            {props.completed}
+        </div>
     )
 }
