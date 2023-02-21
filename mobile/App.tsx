3cac99eef9c9a68e8f92071db94a65c90e736493
pedrob14 <pedrob14farias@gmail.com>
2023-02-21 16:36:57 -0300

    Exemplo de alteração do bundle da aplicação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red'
+  }
 });
